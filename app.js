0de41378ef987de04244a853f6c1a06dbf7bbb62
RyanJIGO <132185867+ryanJigo@users.noreply.github.com>
2026-04-06 22:59:41 -0700

    Update property map locations

--- app.js
+++ app.js
@@ -12,28 +12,28 @@ import 'leaflet/dist/leaflet.css';
     {
       name: "Lightbox",
       address: "4545 8th Ave NE, Seattle, WA 98105",
-      coords: [47.6620, -122.3177],
+      coords: [47.6621, -122.3176],
       rent: "$1,949",
       unit: "Two Bedroom B1A"
     },
     {
       name: "Capella at Esterra Park",
       address: "2710 Tagore Ave, Redmond, WA 98052",
-      coords: [47.6747, -122.1221],
+      coords: [47.6740, -122.1215],
       rent: "$1,975",
       unit: "D2"
     },
     {
       name: "Glendale Apartments",
       address: "5246 Brooklyn Ave NE, Seattle, WA 98105",
-      coords: [47.6640, -122.3147],
+      coords: [47.6641, -122.3145],
       rent: "$1,999",
       unit: "2 Bed 1 Bath"
     },
     {
       name: "East Howe Steps",
       address: "1823 Eastlake Ave E, Seattle, WA 98102",
-      coords: [47.6372, -122.3241],
+      coords: [47.6373, -122.3242],
       rent: "~$2,095+",
       unit: "Eastlake 2BD"
     }
